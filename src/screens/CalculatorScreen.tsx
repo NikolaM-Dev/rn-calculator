05ba297518bc9befc83b651a158ff206aa2967d0
Juan David Merchan Torres <juan1014166@gmail.com>
2021-12-06 17:13:37 -0500

    feat(components): Add calculator snippet

---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text>iOS Calculator</Text>
+    </View>
+  );
+};
+export default CalculatorScreen;
