2f480715b9746a7595c6bf643895d7471725e7b5
Juan David Merchan Torres <juan1014166@gmail.com>
2021-12-09 16:21:17 -0500

    style(screens): Change styles

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
-import {Text, View} from 'react-native';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
 
 import Button from '../components/Button';
-import {styles} from '../theme/appTheme';
+import { styles } from '../theme/appTheme';
 
 const CalculatorScreen = () => {
-  const [number, setNumber] = useState<string>('1500');
+  const [number, setNumber] = useState<string>('0');
   const [ans, setAns] = useState<string>('0');
 
   const clean = () => setNumber('0');
@@ -18,24 +18,19 @@ const CalculatorScreen = () => {
 
     if (number.startsWith('0') || number.startsWith('-0')) {
       // Decimal point
-      if (payload === '.') {
+      if (payload === '.') setNumber(number + payload);
+      // Evaluate if there is zero, and there is a point
+      else if (payload === '0' && number.includes('.'))
         setNumber(number + payload);
-        // Evaluate if there is zero, and there is a point
-      } else if (payload === '0' && number.includes('.')) {
-        setNumber(number + payload);
-        // Evaluate if it is different from zero and has no point
-      } else if (payload !== '0' && !number.includes('.')) {
-        setNumber(payload!);
+      // Evaluate if it is different from zero and has no point
+      else if (payload !== '0' && !number.includes('.')) {
+        if (payload) setNumber(payload);
         // Prevent 000.0
-      } else if (payload === '0' && !number.includes('.')) {
-        return;
-      } else {
-        setNumber(number + payload);
-      }
-    } else {
-      setNumber(number + payload);
-    }
+      } else if (payload === '0' && !number.includes('.')) return;
+      else setNumber(number + payload);
+    } else setNumber(number + payload);
   };
+
   const positveAndNegative = () => {
     number.includes('-')
       ? setNumber(number.replace('-', ''))
@@ -45,54 +40,75 @@ const CalculatorScreen = () => {
   return (
     <View style={styles.wrapper}>
       <Text style={styles.ans}>{ans}</Text>
-      <Text style={styles.result} numberOfLines={1} adjustsFontSizeToFit>
+      <Text
+        adjustsFontSizeToFit
+        numberOfLines={1}
+        style={styles.result}
+        onPress={handleDel}>
         {number}
       </Text>
       <View style={styles.row}>
         <Button
-          text={'C'}
-          color={'black'}
-          backgroundColor={'#A5A5A5'}
           action={clean}
+          backgroundColor={'#A5A5A5'}
+          color={'black'}
+          text={'C'}
         />
         <Button
           action={positveAndNegative}
-          text={'+/-'}
-          color={'black'}
           backgroundColor={'#A5A5A5'}
+          color={'black'}
+          text={'+/-'}
         />
         <Button
-          action={buildResult}
-          text={'%'}
-          color={'black'}
+          action={handleSendToAns}
           backgroundColor={'#A5A5A5'}
+          color={'black'}
+          text={'%'}
         />
-        <Button action={buildResult} text={'/'} backgroundColor={'#F1A33B'} />
+        <Button
+          action={handleSendToAns}
+          backgroundColor={'#F1A33B'}
+          text={'/'}
+        />
       </View>
       <View style={styles.row}>
         <Button action={buildResult} text={'7'} />
         <Button action={buildResult} text={'8'} />
         <Button action={buildResult} text={'9'} />
-        <Button action={buildResult} text={'x'} backgroundColor={'#F1A33B'} />
+        <Button
+          action={handleSendToAns}
+          backgroundColor={'#F1A33B'}
+          text={'x'}
+        />
       </View>
       <View style={styles.row}>
         <Button action={buildResult} text={'6'} />
         <Button action={buildResult} text={'5'} />
         <Button action={buildResult} text={'4'} />
-        <Button action={buildResult} text={'-'} backgroundColor={'#F1A33B'} />
+        <Button
+          action={handleSendToAns}
+          backgroundColor={'#F1A33B'}
+          text={'-'}
+        />
       </View>
       <View style={styles.row}>
         <Button action={buildResult} text={'3'} />
         <Button action={buildResult} text={'2'} />
         <Button action={buildResult} text={'1'} />
-        <Button action={buildResult} text={'+'} backgroundColor={'#F1A33B'} />
+        <Button
+          action={handleSendToAns}
+          backgroundColor={'#F1A33B'}
+          text={'+'}
+        />
       </View>
       <View style={styles.row}>
-        <Button action={buildResult} text={'0'} double />
+        <Button double action={buildResult} text={'0'} />
         <Button action={buildResult} text={'.'} />
-        <Button action={buildResult} text={'='} backgroundColor={'#F1A33B'} />
+        <Button action={buildResult} backgroundColor={'#F1A33B'} text={'='} />
       </View>
     </View>
   );
 };
+
 export default CalculatorScreen;
